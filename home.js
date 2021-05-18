aec96955e3f38b3d13fbf863a1e294c40dfb5128
Léon Lenclos <contact@leonlenclos.net>
2021-05-18 18:03:24 +0200

    import game feature

--- home.js
+++ home.js
@@ -22,6 +22,13 @@ function home(){
 				.mousePressed(continueGame)
 				.parent(panel);
 		}
+
+		createElement('hr')
+			.parent(panel)
+		createSpan('import a game:')
+			.parent(panel)
+		createFileInput(importGame)
+			.parent(panel)
 	}
 	
 	this.enter = function(){
@@ -36,6 +43,44 @@ function home(){
 	this.exit = function (){
 		panel.hide();
 	};
+
+	function importGame(file){
+		let tempDiv = createDiv(file.data).hide()
+		print(tempDiv)
+		let imgmGame = select('#imgmGame', tempDiv)
+		if(imgmGame){
+			let images = selectAll('img', imgmGame);
+			let maps = selectAll('map', imgmGame);
+			let scenes = []
+			for(let i=0; i<images.length; i++){
+				let imageData = images[i].attribute('src')
+				let gates = []
+				let areas = selectAll('area', maps[i])
+				for(let j=0; j<areas.length; j++){
+					let gate = {
+						poly:int(split(areas[j].attribute('coords'), ',')),
+						sceneId:areas[j].attribute('href').substring(1)
+					};
+					gates.push(gate);
+				}
+				let scene = {
+					imageData:imageData,
+					gates:gates
+				}
+				scenes.push(scene);
+			}
+			tempDiv.remove()
+			game = {
+				scenes:scenes
+			};
+			sceneIndex = 0;
+			mgr.showScene(editScene);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 	function newGame(){
 		game = {
